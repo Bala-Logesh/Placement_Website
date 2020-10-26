d8612e86e244169ba05e9cb42ddc7bf61eb39885
Bala-Logesh <balassmp@gmail.com>
2020-10-26 20:25:09 +0530

    first commit

--- Company_Interviews/faq.js
+++ Company_Interviews/faq.js
@@ -4,6 +4,6 @@ function question1() {
         <p>This contains the questions from many Interview Preparation Youtube videos</p>
         <p>You can view or download the file from this Drive link</p>
         <br>
-        <a href="https://drive.google.com/file/d/1pbKDVUHS17MIIVnET2-oZsj7L45hvHi_/view?usp=sharing" style="color: white; text-decoration: none; font-size: 24pt;" target="_blank">Click here for FAQ Interview Questions</a>
+        <a href="https://drive.google.com/file/d/1pbKDVUHS17MIIVnET2-oZsj7L45hvHi_/view?usp=sharing" target="_blank">Click here for FAQ Interview Questions</a>
     `;
 }
